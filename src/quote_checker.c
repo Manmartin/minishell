ad777f2824c2d156281849b5095e9608ea272069
Manmartin <manuelmartinlopez02@gmail.com>
2022-01-10 18:00:15 +0100

    Norm errors fixed

--- src/quote_checker.c
+++ src/quote_checker.c
@@ -6,39 +6,42 @@
 /*   By: manmarti <manmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 14:37:48 by manmarti          #+#    #+#             */
-/*   Updated: 2022/01/10 17:43:13 by manmarti         ###   ########.fr       */
+/*   Updated: 2022/01/10 17:57:19 by manmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+static void	check_flags(t_flags *flags, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'' && !flags->d_qts)
+			flags->s_qts = !flags->s_qts;
+		else if (str[i] == '"' && !flags->s_qts)
+			flags->d_qts = !flags->d_qts;
+		i++;
+	}
+}
+
 bool	quote_checker(t_list *tokens)
 {
 	t_flags	flags;
 	t_list	*first;
-	int		i;
-	char	*aux;
 
-	i = 0;
 	first = tokens;
 	ft_memset(&flags, 0, sizeof(t_flags));
 	while (tokens)
 	{
-		aux = tokens->content;
-		while (aux[i])
-		{
-			if (aux[i] == '\'' && !flags.d_qts)
-				flags.s_qts = !flags.s_qts;
-			else if (aux[i] == '"' && !flags.s_qts)
-				flags.d_qts = !flags.d_qts;
-			i++;
-		}
+		check_flags(&flags, tokens->content);
 		if (flags.d_qts || flags.s_qts)
 		{
 			ft_lstclear(&first, free);
 			return (false);
 		}
-		i = 0;
 		ft_memset(&flags, 0, sizeof(t_flags));
 		tokens = tokens->next;
 	}
