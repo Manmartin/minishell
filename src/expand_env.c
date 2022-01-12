c2ad24ed6c0f56fc20097c2e1b59f958527ad45d
Manmartin <manuelmartinlopez02@gmail.com>
2022-01-12 16:43:20 +0100

    Norm error of expand_env fixed.

--- src/expand_env.c
+++ src/expand_env.c
@@ -6,34 +6,33 @@
 /*   By: manmarti <manmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 18:32:54 by manmarti          #+#    #+#             */
-/*   Updated: 2022/01/12 16:00:41 by manmarti         ###   ########.fr       */
+/*   Updated: 2022/01/12 16:42:37 by manmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+static void	exit_error(char *msg)
+{
+	perror(msg);
+	exit(0);
+}
 
 static char	*append_string(char *s1, char *s2)
 {
 	char	*new_str;
 
 	if (!s1 || !s2)
-	{
-		perror("malloc");
-		exit(0);
-	}
+		exit_error("malloc");
 	new_str = ft_strjoin(s1, s2);
 	free(s1);
 	free(s2);
 	if (!new_str)
-	{
-		free_data();
-		perror("malloc");
-		exit(0);
-	}
+		exit_error("malloc");
 	return (new_str);
 }
 
-static int select_env(char **s, int i)
+static int	select_env(char **s, int i)
 {
 	char	*env_var;
 	char	*aux;
@@ -45,28 +44,16 @@ static int select_env(char **s, int i)
 	str = *s;
 	while (str[i + j] && ft_isenv(str[i + j]))
 		j++;
-	if (j == 0)
-		aux = ft_strdup("$");
-	else
-		aux = ft_substr(str, i, j);
+	aux = ft_substr(str, i, j);
 	if (!aux)
-	{
-		free_data();
-		perror("malloc");
-		exit(0);
-	}
-	if (j == 0)
-		env_var = aux;
-	else
-		env_var = get_env(aux);
+		exit_error("malloc");
+	env_var = get_env(aux);
 	if (!env_var)
-	{
-		free_data();
-		perror("malloc");
-		exit(0);
-	}
+		exit_error("malloc");
+	free(aux);
 	var_len = ft_strlen(env_var);
-	*s = append_string(append_string(ft_substr(str, 0, i - 1), env_var), ft_substr(str, i + j, ft_strlen(&str[i + j])));
+	*s = append_string(append_string(ft_substr(str, 0, i - 1), env_var),
+			ft_substr(str, i + j, ft_strlen(&str[i + j])));
 	free(str);
 	return (i - 1 + var_len);
 }
@@ -74,15 +61,15 @@ static int select_env(char **s, int i)
 void	expand_env(t_list *tokens)
 {
 	char	*aux;
-	int 	i;
+	int		i;
 
 	i = 0;
 	while (tokens)
 	{
 		aux = tokens->content;
-		while(aux[i])
+		while (aux[i])
 		{
-			if (aux[i] == '$')
+			if (aux[i] == '$' && ft_isenv(aux[i + 1]))
 				i = select_env(&aux, i + 1) - 1;
 			i++;
 		}
