fbe928a3404aa1fdde3729b3b309dc30b34f6692
Manmartin <manuelmartinlopez02@gmail.com>
2022-02-28 21:56:45 +0100

    cd fixed

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: acrucesp <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:22:56 by acrucesp          #+#    #+#             */
-/*   Updated: 2022/02/27 20:27:53 by acrucesp         ###   ########.fr       */
+/*   Updated: 2022/02/28 21:51:01 by manmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int	cd(t_cmd *cmd)
 	char	*path;
 	int		size;
 
-	size = ft_strlen(cmd->argv[1]) + ft_strlen(get_env("PWD")) + 2;
+	if (cmd->argc > 3 || cmd->argc == 2)
+		return (0);
+	size = 4096;
 	path = ft_calloc(sizeof(char), size);
 	chdir(cmd->argv[1]);
 	path = getcwd(path, size);
@@ -28,8 +30,20 @@ int	cd(t_cmd *cmd)
 int	cd_exec(t_cmd *cmd)
 {
 	int		error;
+	char	*path;
 
-	error = chdir(cmd->argv[1]);
+	if (cmd->argc == 2)
+		return (0);
+	if (cmd->argc > 3)
+	{
+		ft_putendl_fd("cd: too many arguments", STDERR_FILENO);
+		return (0);
+	}
+	else if (cmd->argv[1] == NULL)
+		path = get_env("HOME");
+	else
+		path = cmd->argv[1];
+	error = chdir(path);
 	if (error == -1)
 	{
 		write(STDERR_FILENO, "cd: ", 4);
