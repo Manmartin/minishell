008a598fef3dffa329994c8588aa3c2767f55724
Manmartin <manuelmartinlopez02@gmail.com>
2022-03-02 12:55:06 +0100

    Relexer works well

---
+++ src/env/aux_expand.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aux_expand.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manmarti <manmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 10:24:12 by manmarti          #+#    #+#             */
+/*   Updated: 2022/03/02 11:00:56 by manmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+int	select_env(char **s, int i)
+{
+	char	*env_var;
+	char	*aux;
+	char	*str;
+	int		var_len;
+	int		j;
+
+	j = 0;
+	str = *s;
+	while (str[i + j] && ft_isenv(str[i + j]))
+		j++;
+	aux = ft_substr(str, i, j);
+	if (!aux)
+		exit_error("malloc");
+	env_var = get_env(aux);
+	if (!env_var)
+		exit_error("malloc");
+	free(aux);
+	var_len = ft_strlen(env_var);
+	*s = append_string(append_string(ft_substr(str, 0, i - 1), env_var),
+			ft_substr(str, i + j, ft_strlen(&str[i + j])));
+	free(str);
+	return (i - 1 + var_len);
+}
+
+char	*make_expanded_str(char **str)
+{
+	t_flags	flags;
+	char	*aux;
+	int		i;
+
+	i = 0;
+	ft_memset(&flags, 0, sizeof(t_flags));
+	aux = *str;
+	while (aux[i])
+	{
+		if (aux[i] == '\'' && !flags.d_qts)
+			flags.s_qts = !flags.s_qts;
+		else if (aux[i] == '"' && !flags.s_qts)
+			flags.d_qts = !flags.d_qts;
+		if (aux[i] == '$' && ft_isenv(aux[i + 1]) && !flags.s_qts)
+			i = select_env(&aux, i + 1) - 1;
+		i++;
+	}
+	return (aux);
+}
+
+void	make_expand(char **str, char *flag)
+{
+	char	*aux;
+
+	aux = ft_strdup(*str);
+	*str = make_expanded_str(str);
+	if (ft_strncmp(aux, *str, ft_strlen(aux) + 1))
+		*flag = 1;
+	free(aux);
+	quote_remover(str);
+}
---
+++ src/env/relexer.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   relexer.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manmarti <manmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 11:04:30 by manmarti          #+#    #+#             */
+/*   Updated: 2022/03/02 12:51:03 by manmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+static void	retokenize(t_cmd **cmd, t_list *aux, int i)
+{
+	int		j;
+
+	j = 0;
+	while (cmd[i]->argv[j])
+		free(cmd[i]->argv[j++]);
+	free(cmd[i]->argv);
+	cmd[i]->argv = ft_calloc(sizeof(char *), ft_lstsize(aux) + 1);
+	j = 0;
+	while (aux)
+	{
+		cmd[i]->argv[j++] = ft_strdup(aux->content);
+		aux = aux->next;
+	}
+}
+
+static void	relexer_args(t_cmd **cmds, int i)
+{
+	int		j;
+	t_list	*aux;
+
+	j = -1;
+	aux = NULL;
+	while (cmds[i]->argv[++j])
+	{
+		if (cmds[i]->was_exp[j])
+			ft_lstadd_back(&aux, lexer(cmds[i]->argv[j]));
+		else
+			ft_lstadd_back(&aux, ft_lstnew(ft_strdup(cmds[i]->argv[j])));
+	}
+	retokenize(cmds, aux, i);
+	ft_lstclear(&aux, free);
+	ft_putendl_fd("Ambiguous redirection", STDERR_FILENO);
+}
+
+static void	rdrc_error(t_cmd *cmd)
+{
+	t_list	*rdrc;
+	t_rdtns	*error_flag;
+
+	rdrc = cmd->rdtns;
+	if (((t_rdtns *)rdrc->content)->type[0] == 'x')
+		return ;
+	ft_lstclear(&cmd->rdtns, free_redirections);
+	error_flag = ft_calloc(sizeof(t_rdtns), 1);
+	error_flag->type = ft_strdup("x");
+	cmd->rdtns = ft_lstnew(error_flag);
+}
+
+static void	relexer_rdrc(t_cmd **cmds, t_list *rdrc, int i)
+{
+	int		j;
+	t_list	*aux;
+
+	aux = NULL;
+	j = 0;
+	while (rdrc)
+	{
+		if (cmds[i]->rwas_exp[j])
+			ft_lstadd_back(&aux, lexer(((t_rdtns *)rdrc->content)->file));
+		else
+			ft_lstadd_back(&aux,
+				ft_lstnew(ft_strdup(((t_rdtns *)rdrc->content)->file)));
+		rdrc = rdrc->next;
+		j++;
+	}
+	if (ft_lstsize(cmds[i]->rdtns) != ft_lstsize(aux))
+		rdrc_error(cmds[i]);
+	rdrc = aux;
+	while (rdrc)
+	{
+		if (rdrc->content == NULL)
+			rdrc_error(cmds[i]);
+		rdrc = rdrc->next;
+	}
+	ft_lstclear(&aux, free);
+}
+
+void	relexer(t_cmd **cmds)
+{
+	int		i;
+	t_list	*rdrc;
+
+	i = -1;
+	while (cmds[++i])
+	{
+		relexer_args(cmds, i);
+		rdrc = cmds[i]->rdtns;
+		relexer_rdrc(cmds, rdrc, i);
+	}
+}
